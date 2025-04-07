63db8a919ddb5b471535f6cf0a4fc66cc3cdd3c6
Queenthishu-md <queenthishu@gmail.com>
2025-04-07 15:53:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~3F8gzTKR#IW8dR79jcNsyv0frIALblbftx6_bJDiMyD7yh7tnAQQ",
+  MONGODB: process.env.MONGODB || "mongodb+srv://queenthishu:s0rb2Eirgl24YY0v@cluster0.bzmsfob.mongodb.net/",
+  OWNER_NUM: process.env.OWNER_NUM || "94761480834",
 };
